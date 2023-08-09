11c43b37eb821211fa721f6e5d9477583425a224
AryanA <aryan.aiyappa@gmail.com>
2023-08-09 23:59:06 +0530

    ChatBot

--- app/src/main/java/com/example/easychatgpt/MainActivity.java
+++ app/src/main/java/com/example/easychatgpt/MainActivity.java
@@ -97,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
         RequestBody body = RequestBody.create(jsonBody.toString(),JSON);
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/completions")
-                .header("Authorization","Bearer YOUR_API_KEY")
+                .header("Authorization","Bearer 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")
                 .post(body)
                 .build();
 
